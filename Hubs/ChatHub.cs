05bf2a1480f6ee5ac2b460690594a25702601eb2
Andrei Epure <andrei.epure@sonarsource.com>
2023-12-13 12:34:34 +0100

    add signal r hub and chatpage

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TodoList.Hubs;
+
+public class ChatHub : Hub
+{
+    private Dictionary<string, string> messages = new();
+
+    public async Task Reload(string connectionId)
+    {
+        await Clients.Client(connectionId).SendAsync("Reload", messages);
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        messages[user] = message;
+
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.AspNetCore.ResponseCompression;
 using TodoList.Components;
+using TodoList.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
@@ -14,6 +22,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // SignalR
+    app.UseResponseCompression();
 }
 
 app.UseHttpsRedirection();
@@ -23,5 +34,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
