225cc4b6c3ef25526bf03ec725b8f5de0cdd315a
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-12-13 13:32:42 +0100

    Add and calculate visualizable properties to PositionView

---
+++ ETFGeoVisualizer/PositionView.cs
@@ -0,0 +1,7 @@
+﻿public class PositionView
+{
+    public string Ticker { get; set; }
+    public string AssetClass { get; set; }
+    public string Description { get; set; }
+    public string WeightInPortfolio { get; set; }
+}
